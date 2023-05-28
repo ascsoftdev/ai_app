51d625fa3fecf178bee4202de6ce66771a6713a5
achauhan <ascsoftdev@gmail.com>
2023-05-28 15:00:28 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/asc/allen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.asc.allen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
